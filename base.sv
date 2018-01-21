99771a085e3aa7f1a8e48b3f9aa32f76456da0ce
urmidebnath <34119817+urmidebnath@users.noreply.github.com>
2018-01-21 13:21:11 +0600

    Update base.sv

--- base.sv
+++ base.sv
@@ -1,19 +1,37 @@
-`define count = 10
-class xname extends transaction;
-  constraint c_size{
-    size == XSIZE;
+`define RUN_COUNT 10
+class xname extends dma_transaction;
+  constraint tr_mode_constraint {
+    tr_mode == XMODE;
   }
-  constraint re{
-    rep == XR;
+
+  constraint size_trans_constraint {
+    size_trans == XSIZE;
   }
-  constraint c_mode{
-    mode == XMODE;
+
+  constraint reload_constraint {
+		re_load  == XR;
   }
-  constraint c_admode{
-    admode == XAD;
+
+  constraint addr_mode_constraint {
+    addr_mode == XAD;
   }
 endclass
 
-program(v_if vif);
-  xname t_or;
+program dma_test ( dma_interface dma_if);
+  dma_environment env;
+  xname tr_os; 
+
+  initial begin
+    env = new(dma_if);
+    env.build();
+
+    begin : config_gen
+			tr_os = new();
+			env.gen.run_count = `RUN_COUNT;
+			env.drv.run_count = `RUN_COUNT;
+			env.gen.blueprint = tr_os;
+    end : config_gen
+
+    env.run();
+  end
 endprogram
